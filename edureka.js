cd27db84e6fa8cedd9443cf8295538c0c554ef47
Rajnish <rajnishpal161@gmail.com>
2021-03-15 18:55:28 +0530

    First Commit

---
+++ edureka.js
@@ -0,0 +1,24 @@
+console.log("hello")
+
+document.write("hello world")
+
+
+var a=[4,5,6]
+
+var a = new Array(3,4,5,6)
+
+console.log(typeof(a))
+
+var b=prompt("enter value")
+
+console.log(parseInt(b)+2)
+
+alert("kuchh nhi! Aisehi")
+
+
+
+const numbers=new Array(1,2,3,4,5),
+
+console.log(numbers)
+
+
