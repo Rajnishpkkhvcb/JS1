2b7368c1652ffab666541511ccb1e1311177a604
Rajnishpkkhvcb <60421387+Rajnishpkkhvcb@users.noreply.github.com>
2021-03-16 12:23:58 +0530

    getElementByClassName

--- dom.js
+++ dom.js
@@ -1,2 +1,30 @@
-console.log(document.domain)
-console.log(document.URL)
+//console.log(document.URL)
+//console.log(document.title)
+//document.title="Rajnish"
+
+//console.log(document.all[2])
+console.log("hii");
+//var head1=document.getElementById('Header');
+//console.log(head1);
+
+//head1.style.backgroundColor="red"
+
+//var x = document.getElementById(Header1);   // Get the element with id="demo"
+//x.style.color = "red";
+
+//var y=document.getElementsByClassName(list-group)
+//y.style.color="red"
+
+
+
+function changeStyling()
+{
+    var element=document.getElementsByClassName("items");
+    for (var x=0;x<element.length;x++)
+    if(x%2==0){
+    {
+        element[x].style.backgroundColor="green"
+        element[x+1].style.fontWeight="bold"
+    }
+}
+}
